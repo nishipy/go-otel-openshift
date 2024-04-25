bf401a2974b2f742c114d05162ebf8733a5389b9
nishipy <goodisonev4@gmail.com>
2024-04-25 22:42:51 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+func main() {
+	ctx := context.Background()
+	exp, err := otlptracegrpc.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	tracerProvider := trace.NewTracerProvider(trace.WithBatcher(exp))
+	defer func() {
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	otel.SetTracerProvider(tracerProvider)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tr := otel.Tracer("http-server")
+		_, span := tr.Start(r.Context(), "handleRequest")
+		defer span.End()
+
+		// Simulate some work
+		time.Sleep(100 * time.Millisecond)
+
+		w.Write([]byte("Hello, world!\n"))
+	})
+
+	// Start the HTTP server
+	port := ":8080"
+	log.Printf("Starting server on port %s...", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
